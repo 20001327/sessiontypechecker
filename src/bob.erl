a2a40e24cd97fa20c641996a56d63467e19a08df
Lorenzo <eis513@hotmail.it>
2021-08-11 15:32:33 +0200

    delegation with registration

--- src/bob.erl
+++ src/bob.erl
@@ -12,16 +12,18 @@ init() ->
     {seller, quote, Quote} ->
       %%io:format("BOB: received QUOTE ~n"),
       receive
-        {alice, myquote, MyQuote} when Quote - MyQuote < 100 ->
-          seller ! {?MODULE, ok},
-          alice ! {?MODULE, ok},
-          seller ! {?MODULE, address, "Address"},
-          receive
-            {seller, date, Date} ->
-              Date
-          %%io:format("BOB: received Date ~n")
-          end;
-        {alice, myquote, MyQuote} when Quote - MyQuote >= 100 ->
+        {alice, myquote, MyQuote} ->
+          case Quote - MyQuote < 100 of
+            true ->
+              seller ! {?MODULE, ok},
+              alice ! {?MODULE, ok},
+              seller ! {?MODULE, address, "Address"},
+              receive
+                {seller, date, Date} ->
+                  Date
+              %%io:format("BOB: received Date ~n")
+              end;
+        false ->
           carol:start(),
           unregister(?MODULE),
           carol ! {?MODULE, start_delegation, {?MODULE, self(), Quote - MyQuote - 99}},
