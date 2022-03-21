bdd3fabd226ca70037fbb964f11d6f672ed342c9
Lorenzo <eis513@hotmail.it>
2022-03-21 18:43:01 +0100

    ricorsione con regole - aggiornate client e seller per CSB

--- examples/seller-bank-no-global/client.erl
+++ examples/seller-bank-no-global/client.erl
@@ -1,30 +1,27 @@
 -module(client).
-
 -export([start/0, init/0]).
-
-start()->
-  register(client,spawn(client, init, [])).
-
+start() -> register(client,spawn(client, init,[])).
 -type init() :: 'seller!title<String>.seller?price<Int>.@(seller!ok.seller?pay<Int>.seller!card<String>.&(seller?date<String>.End,seller?ko.End),seller!ko.End)'.
-init()->
+init() ->
+  MyLimit = 50,
   seller!{client,title,"Novecento"},
   receive
-    {seller, price, Price} ->
-    case Price =< 50 of
+    {seller,price,Price} ->
+    case Price=<MyLimit of
       true ->
           seller!{client,ok},
           receive
-            {seller, pay, Price} ->
+            {seller,pay,Price} ->
               seller!{client,card,"0000000000000000"},
               receive
-                {seller, date, Date} ->
+                {seller,date,Date} ->
                   ok;
-                {seller, ko} ->
+                {seller,ko} ->
                   ko
-              end
+            end
           end;
       false ->
           seller!{client,ko},
           'End'
-      end
-  end.
+    end
+end.
